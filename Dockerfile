6db33921ff1e4333287f0ee6995150bf5e300143
Tri Wardana <mtriwardanaa@gmail.com>
2024-03-09 13:14:53 +0700

    feat: 🎸 init

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:slim
+
+ENV NODE_ENV development
+
+WORKDIR /helloworld
+
+COPY . .
+
+RUN npm install
+
+CMD ["node", "index.js"]
+
+EXPOSE 9000
---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const redis = require("redis");
+const port = 9000;
+
+const mongoString = process.env.DATABASE_URL;
+const redisPort = process.env.REDIS_PORT;
+const redisHost = process.env.REDIS_HOST;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+
+let redisClient;
+
+(async () => {
+  redisClient = redis.createClient({
+    url: `redis://:@${redisHost}:${redisPort}`
+  });
+
+  redisClient.on("error", (error) => console.error(`Error : ${error}`));
+
+  await redisClient.connect();
+
+  console.log('Redis Connected')
+})();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello Welcome to Redis Cache DB and ${process.env.APP_NAME} </h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`Server Started at ${port}`)
+})
