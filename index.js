7a4d0961ec126f2d838f6498296afb5003f738f0
Tri Wardana <mtriwardanaa@gmail.com>
2024-03-12 10:37:52 +0700

    fix: 🐛 jenkins

--- index.js
+++ index.js
@@ -43,5 +43,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server Started at ${port} develop test`)
+  console.log(`Server Started at ${port} develop test inter`)
 })
